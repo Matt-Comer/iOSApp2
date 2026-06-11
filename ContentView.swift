7760eddba5c3c508cefb97da23fe7e3f44e2f169
Matt  Comer <Matt.Comer@triosstudent.com>
2026-06-11 18:56:25 -0400

    Update ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iOSApp2
+//
+//  Created by matthew comer on 2026-06-04.
+//
+
+import SwiftUI
+
+// Main screen
+struct ContentView: View {
+    
+    var body: some View {
+        
+        // Opens the scavenger hunt list
+        SplashScreen()
+          }
+      }
+
+      #Preview {
+          ContentView()
+      }
